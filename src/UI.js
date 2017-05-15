8939b19fbd69c4d260435bc3d1a89743dfcdb067
Tor Hagemann <tohagema@cisco.com>
2017-05-14 17:42:07 -0700

    Initial commit: let's get fonts working, etc.

---
+++ src/UI.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UI = () => (
+	<div className="">
+		<span>Tor Eivind Hagemann</span>
+		<br />
+		<span>+</span>
+		<br />
+		<span>Leah Julianne Simpson</span>
+	</div>
+)
+
+export default UI
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import UI from './UI'
+import './index.css'
+
+ReactDOM.render(<UI />, document.getElementById('root'))
