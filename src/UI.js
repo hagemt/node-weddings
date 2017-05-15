73b31346436c26e1d9aaecc7593e2f24b6824d05
Tor Hagemann <tohagema@cisco.com>
2017-05-14 18:45:42 -0700

    src: add basic wedding info, styles

--- src/UI.js
+++ src/UI.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 const UI = () => (
-	<div className="">
-		<span>Tor Eivind Hagemann</span>
-		<br />
-		<span>+</span>
-		<br />
-		<span>Leah Julianne Simpson</span>
+	<div className="invitation">
+		<img src="http://www.indianladderfarms.com/wp-content/uploads/2013/10/farmpump.jpg" />
+		<p>
+			<span>Please join <a href="mailto:tor.hagemann@gmail.com">Tor Eivind Hagemann</a></span>
+			<br />
+			<span>together with <a href="mailto:smilee62991@gmail.com">Leah Julianne Simpson</a></span>
+			<br />
+			<span>in the afternoon on <a href="https://www.google.com/search?q=weather+12009">Sunday, August 20th, 2017</a></span>
+			<br />
+			<span>at <a href="http://www.indianladderfarms.com/">Indian Ladder Farms</a> (ceremony outside)</span>
+			<br />
+			<span>to share in the first celebration of their marriage.</span>
+		</p>
 	</div>
 )
 
